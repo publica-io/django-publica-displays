74e1f706412e9ac43cf8400c267cf6fd3454cb15
Harshad Bhatia <harshadbhatia2012@gmail.com>
2014-11-27 11:30:09 +1100

    Added view proxy template tag

--- views/templatetags/views_tags.py
+++ views/templatetags/views_tags.py
@@ -33,3 +33,56 @@ def render_linkages(context):
 @register.simple_tag(takes_context=True)
 def linkage(context, linkage):
     return linkage.render(context)
+
+
+def set_template(linkage, template):
+    '''
+    ..TODO.. confirm and change checking of template through template model
+    '''
+    if template:
+        linkage.content_object.template.name = template
+    return linkage
+
+
+@register.simple_tag(takes_context=True)
+def view_proxy(context, proxy_view_slug, *args, **kwargs):
+    '''
+    The method takes as input a proxy view slug which is the view whose widgets have been featured_on_homepage
+    are to be displayed.
+    Currently it displays the top 4 widgets with a specified template which is done through
+    View template
+    '''
+    view = View.objects.get(slug=proxy_view_slug)
+    html = []
+    counter = 0
+    template_1 = kwargs.get('template_1', None)
+    template_2 = kwargs.get('template_2', None)
+    template_3 = kwargs.get('template_3', None)
+    template_4 = kwargs.get('template_4', None)
+
+    for linkage in view.linkages.all():
+        if linkage.content_object.featured_on_homepage:
+            if counter == 0:
+                linkage = set_template(linkage, template_1)
+                
+            elif counter == 1:
+                linkage = set_template(linkage, template_2)
+
+            elif counter == 2:
+                linkage = set_template(linkage, template_3)
+
+            elif counter == 3:
+                linkage = set_template(linkage, template_4)
+
+            elif counter == 4:
+                break
+
+            counter += 1
+            html.append(linkage.render(context))
+
+    widget_proxy = ''.join(html)
+    # Get rest of the content
+    view_content = render_linkages(context)
+
+    return widget_proxy + view_content
+
